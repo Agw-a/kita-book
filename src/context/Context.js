bba589bd3142a6a33ee79e94bf3258aa53809024
Agw-a <lizagwa7@gmail.com>
2022-11-19 12:22:19 +0300

    search compnent

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,19 +1,46 @@
-import React from 'react'
-import { useContext, useEffect, UseState, useCallback } from 'react';
+import React, { useContext, useEffect, useCallback, useState } from 'react';
 
 const URL = "http://openlibrary.org/search.json?title=";
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
     const [searchTerms,  setSearchTerms] = useState("The lost world");
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(true)
+    const [resutTitle, setResultTitle] = useState("");
+
+    const fetchBooks = useCallback(async() => {
+        setLoading(true);
+        try {
+            const response = await  fetch(`${URL}${searchTerms}`);
+            const data = await response.json();
+            console.log(data);
+            const {docs} = data;
+        } catch (error){
+            console.log(error);
+            setLoading(false);
+        };
+
+    }, [searchTerms]);
+
+    useEffect(() => {
+        fetchBooks();
+    }, [searchTerms, fetchBooks]);
+
+    return (
+        <AppContext.Provider value= {{
+            loading, books, setSearchTerms, resutTitle, setResultTitle,
+
+        }}>
+            {children}
+        </AppContext.Provider>
+    )
 }
 
-const Context = () => {
-  return (
-    <div>
-      <h1>Context and getting api data</h1>
-    </div>
-  )
+
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
 }
 
-export default Context
+export {AppContext, AppProvider}
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import '../styles/App.css';
-import LandingPage from '../pages/Landing';
-import SignUp from '../pages/SignUp';
-import SearchLibraryData from '../components/GetLibraryData';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "../styles/App.css";
+import LandingPage from "../pages/Landing";
+import SignUp from "../pages/SignUp";
+import SearchLibraryData from "../components/GetLibraryData";
+import { AppProvider } from "../context/Context";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<LandingPage />}/>
-      <Route path='/Sign-Up' element={<SignUp />}/>
-      <Route  path='/Library' element={<SearchLibraryData />}/>
-    </Routes>
-    </BrowserRouter>
+    <AppProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/Sign-Up" element={<SignUp />} />
+          <Route path="/Library" element={<SearchLibraryData />}>
+          <Route path="book"/>
+          <Route />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AppProvider>
   );
 }
 
